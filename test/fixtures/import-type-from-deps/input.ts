4a729bf847447a836073b08af10e30458a563c9b
Anders Nilsson <nilssonanders79@gmail.com>
2026-01-23 16:30:49 +0100

    small fix

--- test/fixtures/import-type-from-deps/input.ts
+++ test/fixtures/import-type-from-deps/input.ts
@@ -1,6 +1,5 @@
 // @ts-expect-error - test case for external lib types
 import { InterfaceWithFields } from "fake-package";
-// @ts-expect-error - test case
 import { FakePackageType } from "./module-with-import-type";
 
 export type TestType = InterfaceWithFields | FakePackageType;
